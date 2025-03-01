754798b7a1b7d671855c0a48d8c8b16c0ee7c4d0
Cheyenne Zaini <cheyenne@czaini.net>
2025-03-01 03:31:52 -0800

    delete test files

--- run_test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Testing TrueFA with QR code scanning...
-
-REM Start the application and feed it inputs
-(
-echo 1
-ping -n 2 127.0.0.1 > nul
-echo assets\qrtest.png
-) | dist\truefa.exe
